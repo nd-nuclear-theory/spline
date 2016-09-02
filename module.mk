ad1eb2d6851d515c9a4d03bcc890dec8c07b6b4c
Mark A. Caprio <mcaprio-nd-edu>
2016-09-02 14:05:40 -0400

    Generate submodule branch for use as submodule

---
+++ module.mk
@@ -0,0 +1,20 @@
+$(eval $(begin-module))
+
+################################################################
+# unit definitions
+################################################################
+
+module_units_h := 
+module_units_cpp-h := spline wavefunction_basis wavefunction_class
+# module_units_f := 
+module_programs_cpp := h2utils_input.cpp
+# module_programs_f :=
+# module_generated :=
+
+################################################################
+# library creation flag
+################################################################
+
+$(eval $(library))
+
+$(eval $(end-module))
